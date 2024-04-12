30865f916f34aeabe46824a3a24ecf610210c694
Marcin Sadowski <marcin.sadowski@contractors.roche.com>
2024-04-12 10:52:10 +0200

    init

---
+++ application/src/Controller/HomeController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HomeController extends AbstractController
+{
+    #[Route('/', name: 'home')]
+    public function index(): Response
+    {
+        return new Response(<<<EOF
+            <html>
+                <body>
+                <p>hellow world</p>
+                </body>
+            </html>
+            EOF
+        );
+    }
+}
