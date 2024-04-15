47cf4b13bc8f5d0e05a052053840e95f69684735
Marcin Sadowski <msdevouted@gmail.com>
2024-04-15 23:45:22 +0200

    testing communication

---
+++ application/migrations/Version20240415202514.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20240415202514 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE print_io CHANGE input input VARCHAR(2048) NOT NULL, CHANGE output output TEXT DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE print_io CHANGE input input VARCHAR(255) NOT NULL, CHANGE output output VARCHAR(255) DEFAULT NULL');
+    }
+}
